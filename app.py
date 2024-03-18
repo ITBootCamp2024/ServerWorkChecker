79a135316cef3710e245645cfa542cbf289328bc
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-18 21:54:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    
+def make_requests():
+    response = requests.get('https://itclusterpython.onrender.com')
+    response = requests.get('https://itclusterpython2024.onrender.com')
+    response = requests.get('https://itclusterjava.onrender.com/teacher')
+
+schedule.every(100).seconds.do(make_requests)
 
 if __name__ == '__main__':
     app.run()
