f2f85feddd9e240239300fac147365703c1d0484
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-19 00:28:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ def make_requests2():
 
 def make_requests3():
     return requests.get('https://itclusterpython2024.onrender.com').content
-    
+
 @app.route('/')
 def hello_world():
-    while(True):
-        return str(make_requests1()) + str(make_requests2()) + str(make_requests3())
+    return str(make_requests1()) + str(make_requests2()) + str(make_requests3())
+
+if __name__ == '__main__':
+    schedule.every(100).seconds.do(make_requests1)
+    schedule.every(100).seconds.do(make_requests2)
+    schedule.every(100).seconds.do(make_requests3)
+    app.run()
