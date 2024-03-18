0533f14fbf6bb5cfe1ad0c5d0aa8327517502a69
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-18 23:31:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def make_requests1():
     return requests.get('https://itclusterpython.onrender.com').content
 
+def make_requests2():
+    return requests.get('itclusterjava.onrender.com/teacher').content
+
+def make_requests3():
+    return requests.get('https://itclusterpython2024.onrender.com').content
+    
 @app.route('/')
 def hello_world():
    return str(make_requests1())
