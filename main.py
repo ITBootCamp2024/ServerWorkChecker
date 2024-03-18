45d10cfa8640ffa4c4dcc6f1aa7a8cbbb151ad22
olgasherbiena <60242066+olgasherbiena@users.noreply.github.com>
2024-03-18 21:23:01 +0200

    init 1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import time
 import requests
+from flask import Flask
+import schedule
 
-while True:
-    timing = time.time()
-    if time.time() - timing > 100.0:
-        timing = time.time()
-        response = requests.get('https://itclusterpython.onrender.com')
-        response = requests.get('https://itclusterpython2024.onrender.com')
-        response = requests.get('https://itclusterjava.onrender.com/teacher')
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+def make_requests():
+    response = requests.get('https://itclusterpython.onrender.com')
+    response = requests.get('https://itclusterpython2024.onrender.com')
+    response = requests.get('https://itclusterjava.onrender.com/teacher')
+
+schedule.every(100).seconds.do(make_requests)
+
+if __name__ == '__main__':
+    app.run()
