46feb61c6a2712cbb3e9362e058384e3bddedf87
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-18 23:21:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import schedule
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 def make_requests1():
     return requests.get('https://itclusterpython.onrender.com').content
 
@@ -18,11 +14,10 @@ def make_requests2():
 def make_requests3():
     return requests.get('https://itclusterjava.onrender.com/teacher').content
 
-if __name__ == '__main__':
+@app.route('/')
+def hello_world():
     schedule.every(100).seconds.do(make_requests1)
     schedule.every(100).seconds.do(make_requests2)
     schedule.every(100).seconds.do(make_requests3)
-    
-    while True:
-        schedule.run_pending()
-        time.sleep(1)  # Затримка, щоб програма не використовувала занадто багато CPU
+
+
