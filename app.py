c07e3ffdf53213931c75934d42a59d9802e0df0e
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-18 22:41:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ import schedule
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
     
-def make_requests():
-    response = requests.get('https://itclusterpython.onrender.com')
-    response = requests.get('https://itclusterpython2024.onrender.com')
-    response = requests.get('https://itclusterjava.onrender.com/teacher')
+def make_requests1():
+    return str(requests.get('https://itclusterpython.onrender.com').text)
+def make_requests2():
+    return str(requests.get('https://itclusterpython2024.onrender.com').text)
+def make_requests3():
+    return str(requests.get('https://itclusterjava.onrender.com/teacher').text)
 
-schedule.every(100).seconds.do(make_requests)
+schedule.every(100).seconds.do(make_requests1)
+schedule.every(100).seconds.do(make_requests2)
+schedule.every(100).seconds.do(make_requests3)
 
 if __name__ == '__main__':
     app.run()
