05792b40f0b96b75be734dd0b2ec97a5bd07ede1
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-18 22:47:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,16 @@ def hello_world():
     return 'Hello World!'
     
 def make_requests1():
-    return str(requests.get('https://itclusterpython.onrender.com').text)
+    response = requests.get('https://itclusterpython.onrender.com')
+    app.logger.info('Request to itclusterpython.onrender.com: %s', response.status_code)
+    
 def make_requests2():
-    return str(requests.get('https://itclusterpython2024.onrender.com').text)
+    response = requests.get('https://itclusterpython2024.onrender.com')
+    app.logger.info('Request to itclusterpython2024.onrender.com: %s', response.status_code)
+    
 def make_requests3():
-    return str(requests.get('https://itclusterjava.onrender.com/teacher').text)
+    response = requests.get('https://itclusterjava.onrender.com/teacher')
+    app.logger.info('Request to itclusterjava.onrender.com/teacher: %s', response.status_code)
 
 schedule.every(100).seconds.do(make_requests1)
 schedule.every(100).seconds.do(make_requests2)
