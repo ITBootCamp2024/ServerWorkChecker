ead182aef9899524ba716b338076c1a3ad33b7e8
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-26 00:26:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-import threading
-import time
-import requests
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    filename = 'images/1618010991_9-p-bushuyushchee-more-fentezi-9.jpg'
-    return send_file(filename, mimetype='image/jpg')
+@app.route('/download')
+def download_file():
+    file_path = 'images/1618010991_9-p-bushuyushchee-more-fentezi-9.jpg'
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
