ce7ca80ec6e6101a4e1559290f19f3fba8416390
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-03-26 00:44:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import requests
 from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/download')
 def download_file():
-    file_path = 'https://github.com/ITBootCamp2024/ServerWorkChecker/blob/main/1618010991_9-p-bushuyushchee-more-fentezi-9.jpg'
-    return send_file(file_path, as_attachment=True)
+    # Отримання файлу з GitHub
+    github_file_url = 'https://raw.githubusercontent.com/ITBootCamp2024/ServerWorkChecker/main/1618010991_9-p-bushuyushchee-more-fentezi-9.jpg'
+    response = requests.get(github_file_url)
+
+    # Перевірка статусу відповіді
+    if response.status_code == 200:
+        # Збереження файлу локально
+        with open('downloaded_file.jpg', 'wb') as f:
+            f.write(response.content)
+        
+        # Надсилання файлу користувачеві
+        return send_file('downloaded_file.jpg', as_attachment=True)
+    else:
+        return 'Помилка при завантаженні файлу з GitHub'
 
 if __name__ == '__main__':
     app.run(debug=True)
